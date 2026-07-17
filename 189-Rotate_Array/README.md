5d938d039938a82089a757401ade0240be4b9466
admyazeed <89387525+admyazeed@users.noreply.github.com>
2026-07-17 19:05:17 +0200

    Solve 189 - Rotate Array

---
+++ 189-Rotate_Array/README.md
@@ -0,0 +1,8 @@
+# Solution
+Reverse the entire array, then reverse the first `k` elements of the array, then reverse the remaining (nums.length - k) elements.
+
+## Time complexity
+O(n)
+
+## Space complexity
+O(1)
---
+++ 189-Rotate_Array/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int size = nums.length;
+        k = k % size;
+        reverse(nums, 0, size-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, size-1);
+    }
+
+    private void reverse(int[] arr, int start, int end){
+        while (start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
